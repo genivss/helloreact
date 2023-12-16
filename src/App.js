7aa413520ecabc435e2c8dba403d6a20d7ec95ec
genivss <genivanss@gmail.com>
2023-12-15 23:01:48 -0300

    Implementação de contador com useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import { useState } from 'react';
 
 function App() {
+
+  const [contador, setContador] = useState(0);
+
+  function handleButtonClick(){
+    setContador(contador+1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title={contador} />
+      <input type='button' value="Click Me!" onClick={handleButtonClick} />
     </div>
   );
 }
